b138e80ba005495f3df05b6c441bd52757ab11b2
nv08-8 <nhuvonguyen2005@gmail.com>
2025-09-24 21:33:18 +0700

    frontend
    book_detail

--- bookstore-detail/src/main/java/bookstore/BookstoreApplication.java
+++ bookstore-detail/src/main/java/bookstore/BookstoreApplication.java
@@ -2,10 +2,17 @@ package bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookstoreApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BookstoreApplication.class, args);
-    }
+public class BookstoreApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BookstoreApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
 }
