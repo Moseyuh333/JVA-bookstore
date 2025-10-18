165daa21a1a6d83693fd39992255e3e470ca193d
nv08-8 <nhuvonguyen2005@gmail.com>
2025-10-18 20:06:59 +0700

    Update AdCategoryController.java

--- src/main/java/controller/admin/AdCategoryController.java
+++ src/main/java/controller/admin/AdCategoryController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AdCategoryController
  */
-@WebServlet("/admin-category")
+@WebServlet("/admin-categories")
 public class AdCategoryController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
