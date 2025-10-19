89b95e157c3b980308490350b04db46b9cabf449
nv08-8 <nhuvonguyen2005@gmail.com>
2025-10-20 02:23:20 +0700

    Update AdminCategoriesServlet.java

--- src/main/java/web/admin/AdminCategoriesServlet.java
+++ src/main/java/web/admin/AdminCategoriesServlet.java
@@ -19,7 +19,9 @@ public class AdminCategoriesServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/json");
+        resp.setContentType("application/json; charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        req.setCharacterEncoding("UTF-8");
         PrintWriter out = resp.getWriter();
 
         String action = req.getParameter("action");
@@ -41,7 +43,10 @@ public class AdminCategoriesServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/json");
+        resp.setContentType("application/json; charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        req.setCharacterEncoding("UTF-8");
+
         PrintWriter out = resp.getWriter();
 
         String action = req.getParameter("action");
