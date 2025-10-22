b6147f0c9b0e2041a922e4a9059d99ae333121cd
Nguyen <nguyenthithanhnguyen287@gmail.com>
2025-10-22 10:04:22 +0700

    up

--- src/main/java/web/Seller/SellerDashboardServlet.java
+++ src/main/java/web/Seller/SellerDashboardServlet.java
@@ -1,7 +1,6 @@
-package web.Seller;
-
+package web.seller;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
