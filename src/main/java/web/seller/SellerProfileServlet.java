ecaf55f41375d3a6d97aadc15f473bb424beb5d1
Nguyen <nguyenthithanhnguyen287@gmail.com>
2025-10-24 09:31:13 +0700

    k

---
+++ src/main/java/web/seller/SellerProfileServlet.java
@@ -0,0 +1,16 @@
+package web.seller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/seller/profile")
+public class SellerProfileServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/webapp/Seller/SellerProfile.jsp");
+        dispatcher.forward(request, response);
+    }
+}
---
+++ src/main/webapp/Seller/SellerProfile.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Trang Hồ sơ Seller</title>
+</head>
+<body>
+<h1>Xin chào, đây là trang Hồ sơ của Seller!</h1>
+
+<p>Nội dung test hiển thị thành công.</p>
+
+<a href="${pageContext.request.contextPath}/seller/dashboard">← Quay lại Dashboard</a>
+</body>
+</html>
