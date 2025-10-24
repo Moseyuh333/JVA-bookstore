cd3c9d85451db0c5b6c1faf27f675bdb64065107
Nguyen <nguyenthithanhnguyen287@gmail.com>
2025-10-24 16:20:06 +0700

    .

--- src/main/java/controller/seller/SellerProductController.java
+++ src/main/java/controller/seller/SellerProductController.java
@@ -1,35 +1,16 @@
 package controller.seller;
 
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AdProductController
- */
-@WebServlet("/seller-products")
+@WebServlet("/seller/products")
 public class SellerProductController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	protected void processRequest(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		resp.setContentType("text/html");
-		resp.setCharacterEncoding("UTF-8");
-		req.setCharacterEncoding("UTF-8");
-
-	req.getRequestDispatcher("/Seller/SellerProduct.jsp").forward(req, resp);
-	}
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		processRequest(req, resp);
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		processRequest(req, resp);
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/Seller/SellerProduct.jsp");
+        dispatcher.forward(request, response);
+    }
 }
