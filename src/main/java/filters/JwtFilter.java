18b73abe8639d350c0d5a978aef239be224e353e
nv08-8 <nhuvonguyen2005@gmail.com>
2025-10-26 11:07:23 +0700

    Update JwtFilter.java

--- src/main/java/filters/JwtFilter.java
+++ src/main/java/filters/JwtFilter.java
@@ -77,6 +77,7 @@ public class JwtFilter implements Filter {
             case "/api/admin/clear-users":
             case "/api/test-email":
             case "/api/health":
+            case "/api/seller/register-shop":
                 return true;
             default:
                 break;
